6bd7b30be7c2615179da5aa7a1f80c7148eafc03
Dean Burgess <dinocastiglione@ymail.com>
2019-07-21 19:48:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,7 @@ def introduction(name)
 end 
 introduction("name")
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+end
+introduction_with_language("name", "language")
